c0f9c629829d1aa1eb6edda796c9af4311854ffe
Dendi Handian <dendihandian@gmail.com>
2018-05-23 13:35:20 +0700

    introducing firebase

---
+++ src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyAQU925b3QND90mQuFFunvvIS8dSUXXEeE",
+  authDomain: "turnouts-8c54e.firebaseapp.com",
+  databaseURL: "https://turnouts-8c54e.firebaseio.com",
+  projectId: "turnouts-8c54e",
+  storageBucket: "turnouts-8c54e.appspot.com",
+  messagingSenderId: "716852698784"
+}
+
+export const firebaseApp = firebase.initializeApp(config)
